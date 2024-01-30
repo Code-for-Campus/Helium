5f98cc93b52d2abc1e5ec5c809afc86d10325b13
Owen Wood <owenwood0101@icloud.com>
2024-01-29 20:43:08 -0500

    feat: initial commit

---
+++ src/app/+html.tsx
@@ -0,0 +1,30 @@
+import { ScrollViewStyleReset } from 'expo-router/html';
+
+export default function Root({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Helium</title>
+
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+        <ScrollViewStyleReset />
+
+        <style dangerouslySetInnerHTML={{ __html: responsiveBackground }} />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
+
+const responsiveBackground = `
+body {
+  background-color: #FFFFFF;
+}
+@media (prefers-color-scheme: dark) {
+  body {
+    background-color: #000000;
+  }
+}`;
---
+++ src/app/_layout.tsx
@@ -0,0 +1,44 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+import {SafeAreaProvider} from "react-native-safe-area-context";
+import {Stack} from "expo-router";
+import {StatusBar} from "expo-status-bar";
+
+export {
+  ErrorBoundary,
+} from 'expo-router';
+
+SplashScreen.preventAutoHideAsync();
+
+export default () => {
+  const [loaded, error] = useFonts({
+    Montserrat: require('@/src/assets/fonts/Montserrat.ttf'),
+    ...FontAwesome.font,
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+        <SafeAreaProvider>
+            <StatusBar style='auto'/>
+            <Stack screenOptions={{
+              headerShown: false
+            }}/>
+        </SafeAreaProvider>
+  );
+}
